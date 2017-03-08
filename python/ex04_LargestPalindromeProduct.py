4fb9ddec599908eef26c20f605e7024fcca2b99f
IAMJINU <iamprogrammer.jinu@gmail.com>
2017-03-09 06:40:29 +0900

    add problem 4 context, and measure execution time

--- python/ex04_LargestPalindromeProduct.py
+++ python/ex04_LargestPalindromeProduct.py
@@ -1,28 +1,38 @@
 #ex04_LargestPalindromeProduct.py
+#time :  0.8152897357940674
+#value : 906609(993 x 913)
+
+import time
 
 def findLargestPalindromeProduct():
-    productNumber = 0
-    largestPalindromeProduct = 0
+    productNumber = 0 #save product result
+    largestPalindromeProduct = 0 #save largest palindrome product
     findFirstNumber = 0
     findSecondNumber = 0
-    returnValue = 0
+    returnValue = 0 #has value 1 that if product number is palindrome product
+    measuretime = 0
 
     firstNumber = 999
 
+    measuretime = time.time()
+
+    #first loop
     while firstNumber >= 100:
         secondNumber = 999
 
+        #second loop
         while secondNumber >= 100:
             productNumber = firstNumber * secondNumber
             convertIntToString = str(productNumber)
             lengthString = len(convertIntToString)
 
+            #check if product number length is even
             if(lengthString%2 == 0) :
                 IsPalindromeCnt = 0
                 returnValue = 1
 
+                #check if product number is palindrome number
                 while(IsPalindromeCnt < (lengthString//2)):
-                    #print(convertIntToString[IsPalindromeCnt], convertIntToString[lengthString - 1 - IsPalindromeCnt])
                     if(convertIntToString[IsPalindromeCnt] != convertIntToString[lengthString - 1 - IsPalindromeCnt]):
                         returnValue = 0
                         break;
@@ -32,18 +42,18 @@ def findLargestPalindromeProduct():
             if(returnValue != 0):
                 returnValue = 0
                 if (largestPalindromeProduct < productNumber):
-                    print(largestPalindromeProduct, productNumber)
                     largestPalindromeProduct = productNumber
                     findFirstNumber = firstNumber
                     findSecondNumber = secondNumber
-                #break;
 
             secondNumber -= 1
 
         firstNumber -= 1
 
-    print("\n", findFirstNumber, findSecondNumber)
-    print("\n", largestPalindromeProduct)
+    print(findFirstNumber, findSecondNumber)
+    print(largestPalindromeProduct)
+
+    print("time : ", time.time() - measuretime)
 
 if __name__ == "__main__":
     findLargestPalindromeProduct()
