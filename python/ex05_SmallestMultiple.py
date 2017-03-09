248eb54f7b1947d01c33d9f5a6faa3c3fd6a803c
IAMJINU <iamprogrammer.jinu@gmail.com>
2017-03-10 06:30:56 +0900

    add result value and execution time. but execution time is very long... need to modify algorithm...

--- python/ex05_SmallestMultiple.py
+++ python/ex05_SmallestMultiple.py
@@ -1,8 +1,15 @@
 #ex05_SmallestMultiple.py
+#time :  184.64769291877747
+#value : 232792560
+#this code is not good. execution time is very long...
+#need to modify algorithm...
+
+import time
 
 def findSmallestMultiple():
     findSmallestNumber = 20
     IsPossiabledivide = 0
+    measureTime = time.time()
 
     while(1):
         for number in range(2, 20):
@@ -16,8 +23,9 @@ def findSmallestMultiple():
             print("smallest Number is", findSmallestNumber)
             break
         else:
-            print("searching...(number :", findSmallestNumber, ")")
             findSmallestNumber += 1
+
+    print("time : ", time.time() - measureTime)
 
 if __name__ == "__main__":
     findSmallestMultiple()
